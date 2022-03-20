b1aa51a8ba0401301405f47ea657b424d1b5abc7
Alexey Rodionov <leksurion@gmail.com>
2022-03-20 22:42:24 +0300

    Implement First Screen

--- src/screens/ShoppingList/Calendar/index.tsx
+++ src/screens/ShoppingList/Calendar/index.tsx
@@ -4,6 +4,7 @@ import {useTranslation} from 'react-i18next';
 import s from './styles';
 import CalendarSvg from '../../../assets/images/calendar.svg';
 import ActiveCalendarSvg from '../../../assets/images/calendar-active.svg';
+import {Calendar as DefaultCalendar} from 'react-native-calendars';
 
 const Calendar = () => {
   const {t} = useTranslation();
@@ -11,10 +12,59 @@ const Calendar = () => {
   const Icon = a ? ActiveCalendarSvg : CalendarSvg;
   return (
     <View style={s.container}>
-      <Text>Shopping List Calendar</Text>
-      <Text>{t('shoppingList.calendar.test')}</Text>
-      <Icon width={32} height={32} />
-      <Button title={'Click'} onPress={() => setA(prev => !prev)} />
+      <DefaultCalendar
+        // Initially visible month. Default = now
+        current={'2021-01-01'}
+        // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+        minDate={'2012-05-10'}
+        // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+        maxDate={'2099-12-31'}
+        // Handler which gets executed on day press. Default = undefined
+        onDayPress={day => {
+          console.log('selected day', day);
+        }}
+        // Handler which gets executed on day long press. Default = undefined
+        onDayLongPress={day => {
+          console.log('selected day', day);
+        }}
+        // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+        monthFormat={'yyyy MM'}
+        // Handler which gets executed when visible month changes in calendar. Default = undefined
+        onMonthChange={month => {
+          console.log('month changed', month);
+        }}
+        // Hide month navigation arrows. Default = false
+        hideArrows={true}
+        // Replace default arrows with custom ones (direction can be 'left' or 'right')
+        renderArrow={direction => <Arrow />}
+        // Do not show days of other months in month page. Default = false
+        hideExtraDays={true}
+        // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
+        // day from another month that is visible in calendar page. Default = false
+        disableMonthChange={true}
+        // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
+        firstDay={1}
+        // Hide day names. Default = false
+        hideDayNames={false}
+        // Show week numbers to the left. Default = false
+        showWeekNumbers={false}
+        // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+        onPressArrowLeft={subtractMonth => subtractMonth()}
+        // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+        onPressArrowRight={addMonth => addMonth()}
+        // Disable left arrow. Default = false
+        disableArrowLeft={true}
+        // Disable right arrow. Default = false
+        disableArrowRight={true}
+        // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
+        disableAllTouchEventsForDisabledDays={true}
+        // Replace default month and year title with custom one. the function receive a date as parameter
+        renderHeader={date => {
+          /*Return JSX*/
+        }}
+        // Enable the option to swipe between months. Default = false
+        enableSwipeMonths={true}
+      />
     </View>
   );
 };
